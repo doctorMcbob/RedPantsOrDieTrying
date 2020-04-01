411aeec9534799ad394bd3c1c247dff372c1204f
doctorMcbob <wootenwesley@gmail.com>
2020-04-01 00:26:28 -0700

    platforms

--- level_editor.py
+++ level_editor.py
@@ -2,7 +2,7 @@
 Level editor
 
 right now im just going for platforms
-check out the redpantsadventure repo to see what im going for 
+check out the redpantsadventure repo to see what im going for [spaghetti warning]
 https://github.com/doctorMcbob/RedPantsAdventure/blob/master/leveleditor.py
 
 platforms:
@@ -55,11 +55,12 @@ GAME_STATE[const.LEVEL] = LEVEL
 GAME_STATE[const.GAME_CLOCK] = pygame.time.Clock()
 font = GAME_STATE[const.FONTS][const.FONT_HELVETICA] = pygame.font.SysFont("Helvetica", 16)
 
-CURSOR = [0, 0]
-
+CURSOR = [GAME_STATE[const.WIDTH] / 2, GAME_STATE[const.HEIGHT] / 2]
+CORNER = None
 
 def get_surface(level):
-    scroll = GAME_WORLD.state[const.SCROLL]
+    w = GAME_STATE[const.WIDTH] / 2
+    h = GAME_STATE[const.HEIGHT] / 2
     surf = Surface((
         GAME_STATE[const.WIDTH],
         GAME_STATE[const.HEIGHT]
@@ -69,29 +70,44 @@ def get_surface(level):
         surface = Surface((plat[2], plat[3]))
         surface.fill([(100, 100, 100), (200, 100, 100), (100, 200, 100), (100, 100, 200)][plat[4]])
         surface.blit(font.render("Platform", 0, (0, 0, 0)), (0, 0))
-        surf.blit(surface, (plat[0] + scroll[0], plat[1] + scroll[1]))
+        surf.blit(surface, (plat[0], plat[1]))
+    if CORNER is not None: surf.blit(font.render("C", 0, (0, 0, 0)), (CORNER[0], CORNER[1]))
     return surf
 
 def draw_cursor():
-    w = GAME_STATE[const.WIDTH] / 2
-    h = GAME_STATE[const.HEIGHT] / 2
-    pygame.draw.line(GAME_STATE[const.SCREEN], (255, 0, 0), (w, h), (w+32, h+32), 2)
-    pygame.draw.line(GAME_STATE[const.SCREEN], (255, 0, 0), (w, h+32), (w+32, h), 2)
+    pygame.draw.line(GAME_STATE[const.SCREEN], (255, 0, 0), (CURSOR[0], CURSOR[1]), (CURSOR[0]+32, CURSOR[1]+32), 2)
+    pygame.draw.line(GAME_STATE[const.SCREEN], (255, 0, 0), (CURSOR[0], CURSOR[1]+32), (CURSOR[0]+32, CURSOR[1]), 2)
         
+
+def make_platform(level):
+    global CORNER
+    if CORNER is None:
+        CORNER = CURSOR.copy()
+        return
+    x = min(CURSOR[0], CORNER[0])
+    y = min(CURSOR[1], CORNER[1])
+    w = abs(CURSOR[0] - CORNER[0]) + 32
+    h = abs(CURSOR[1] - CORNER[1]) + 32
     
+    level[const.PLATFORMS].append([x, y, w, h, 1])
+
+    CORNER = None
+
 while True:
     for e in pygame.event.get():
         if e.type == QUIT or e.type == KEYDOWN and e.key == K_ESCAPE: quit()
         if e.type == KEYDOWN:
-            if e.key == K_RIGHT: CURSOR[0] -= 32
-            if e.key == K_LEFT: CURSOR[0] += 32
-            if e.key == K_UP: CURSOR[1] += 32
-            if e.key == K_DOWN: CURSOR[1] -= 32
+            if e.key == K_RIGHT: CURSOR[0] += 32
+            if e.key == K_LEFT: CURSOR[0] -= 32
+            if e.key == K_UP: CURSOR[1] -= 32
+            if e.key == K_DOWN: CURSOR[1] += 32
+            
+            if e.key == K_SPACE: make_platform(LEVEL)
 
-    GAME_WORLD.state[const.SCROLL] = CURSOR
     GAME_STATE[const.SCREEN].blit(get_surface(LEVEL), (0, 0))
     draw_cursor()
     pygame.display.update()
+
     
 
     
